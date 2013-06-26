ad57e26439670f0c7889e21bcc3f1bab6782d91d
Bergunder <jbsgcreate@gmail.com>
2013-06-26 10:17:21 -0700

    made external javascript page

---
+++ script/cartInformation.js
@@ -0,0 +1,18 @@
+var moneyNum = 0;
+var itemsNum = 0;
+						
+	function itemPlus() {
+		itemsNum += 1;
+		document.getElementById("items").innerHTML = "Items: " + itemsNum;
+			
+		moneyNum += 129;
+		document.getElementById("money").innerHTML = "Total: $" + moneyNum;
+		}
+				
+	function itemSale() {
+		itemsNum += 1;
+		document.getElementById("items").innerHTML = "Items: " + itemsNum;
+				
+		moneyNum += 119;
+		document.getElementById("money").innerHTML = "Total: $" + moneyNum;
+		}
